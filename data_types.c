140175cd544c524126ff915825cb6bc7235facec
Gaurav Rajput <grv0908@gmail.com>
2022-08-15 14:26:36 +0530

    Create data_types.c

---
+++ data_types.c
@@ -0,0 +1,16 @@
+// This programs tell about size of each data types
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    char b;
+    float c;
+    double d;
+    
+    printf("Size of int is : %ld\n", sizeof(a));
+    printf("Size of char is : %ld\n", sizeof(b));
+    printf("Size of float is : %ld\n", sizeof(c));
+    printf("Size of double is : %ld", sizeof(d));
+    return 0;
+}
